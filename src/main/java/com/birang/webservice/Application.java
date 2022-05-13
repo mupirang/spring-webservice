ba212ab88ddb88aab6c93d77fc4a757212172ce1
hjlee <hj.lee@tbell.co.kr>
2022-05-13 09:13:34 +0900

    Nginx 적용

--- src/main/java/com/birang/webservice/Application.java
+++ src/main/java/com/birang/webservice/Application.java
@@ -2,13 +2,25 @@ package com.birang.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/spring-webservice/real-application.yml"; //EC2 경로
+			//+ "/Users/hjlee/app/config/spring-webservice/real-application.yml"; //로컬 경로
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(Application.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
+
+	//public static void main(String[] args) {
+	//	SpringApplication.run(Application.class, args);
+	//}
 }
--- src/main/java/com/birang/webservice/web/WebRestController.java
+++ src/main/java/com/birang/webservice/web/WebRestController.java
@@ -3,10 +3,13 @@ package com.birang.webservice.web;
 import com.birang.webservice.domain.ports.PostsRepository;
 import com.birang.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
@@ -14,6 +17,7 @@ public class WebRestController {
 
     private PostsRepository postsRepository;
     private PostsService postsService;
+    private Environment env;
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
@@ -27,5 +31,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
         return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
